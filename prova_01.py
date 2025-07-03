41f79979682947a7c3e6f12320329bba3c6610aa
mariac1995 <camilacontako.fiscal@gmail.com>
2025-07-03 13:11:53 -0300

    Primeiro commit

---
+++ prova_01.py
@@ -0,0 +1,8 @@
+# Crie uma lista contendo seis frutas de sua escolha. Depois de ter a lista pronta, converta 
+# essa lista em uma tupla. Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas. 
+
+lista_frutas = ["banana", "laranja", "manga", "jabuticaba","melancia", "uva"]
+
+tupla_conversao = tuple(lista_frutas)
+
+print(tupla_conversao)
